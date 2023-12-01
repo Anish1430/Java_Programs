30e588b7230bd19fc1a3655710e7798d766441dc
Anish Kumar Sharma <anish.kumar20071998@gmail.com>
2023-12-01 14:01:16 +0530

    RotateArrayByKthStepsInLeft

---
+++ RotateArrayByKthStepsInLeft.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class RotateArrayByKthStepsInLeft {
+
+    public static void rotate(int arr[], int n, int k) {
+        reverse(arr, 0, n - 1);
+        reverse(arr, 0, n - k - 1);
+        reverse(arr, n - k, n - 1);
+    }
+
+    public static void reverse(int arr[], int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        rotate(arr, n, k);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
