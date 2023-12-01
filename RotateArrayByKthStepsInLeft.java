97f82c7d71a02480f2776f08d0d5c3c8afdbb5d8
Anish Kumar Sharma <anish.kumar20071998@gmail.com>
2023-12-01 14:02:25 +0530

    RotateArrayByKthStepsInLeft

--- RotateArrayByKthStepsInLeft.java
+++ RotateArrayByKthStepsInLeft.java
@@ -1,5 +1,44 @@
 import java.util.*;
 
+/*
+You have been given a random integer array/list(ARR) of size N.
+Write a function that rotates the given array/list by D elements(towards the left).
+
+Note: Change in the input array/list itself.
+Input Format
+
+First line of each test case or query contains an integer 'N' representing the size of the array/list.
+
+Second line contains 'N' single space separated integers representing the elements in the array/list.
+
+Third line contains the value of 'D' by which the array/list needs to be rotated.
+
+Constraints
+
+0 <= N <= 10^6 0 <= D <= N
+
+Output Format
+
+print the rotated array/list in a row separated by a single space.
+
+Sample Input 0
+
+7
+1 2 3 4 5 6 7
+2
+Sample Output 0
+
+3 4 5 6 7 1 2
+Sample Input 1
+
+7
+1 2 3 4 5 6 7
+0
+Sample Output 1
+
+1 2 3 4 5 6 7
+ */
+
 public class RotateArrayByKthStepsInLeft {
 
     public static void rotate(int arr[], int n, int k) {
