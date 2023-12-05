96146c54ea6ea508ec2f9d5357aaf681c12a037d
Anish Kumar Sharma <anish.kumar20071998@gmail.com>
2023-12-05 14:03:02 +0530

    InsertionSort

---
+++ InsertionSort.java
@@ -0,0 +1,63 @@
+
+/*
+Take an array of size N as input.
+
+Sort the array using insertion sort.
+
+Note: Don't use inbuilt sort function.
+
+Input Format
+
+An integer N, which is the size of the array.
+
+N integers, depicting the elements of the array.
+
+Constraints
+
+0 <= arr.length <= 10^4
+
+-1*10^5 <= arr[i] <= 10^5
+Output Format
+
+Return A sorted array of size N.
+
+Sample Input 0
+
+5
+9 4 3 8 7
+Sample Output 0
+
+3 4 7 8 9
+
+ */
+
+import java.util.*;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++) {
+            arr[i]=sc.nextInt();
+        }
+        int ans[]=insertionSort(arr,n);
+        for(int i=0;i<ans.length;i++) {
+            System.out.print(ans[i]+" ");
+        }
+    }
+
+    private static int[] insertionSort(int[] arr, int n) {
+        for(int i=1; i<arr.length; i++) {
+            int current = arr[i];
+            int j = i - 1;
+            while(j >= 0 && arr[j] > current) {
+                //Keep swapping
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = current;
+        }
+   return arr;
+    }
+}
