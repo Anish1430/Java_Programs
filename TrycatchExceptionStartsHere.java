823ee06ff2e120e254041f36df847cf00a5ec644
Anish Kumar Sharma <anish.kumar20071998@gmail.com>
2024-02-10 15:30:38 +0530

    "try catch"

--- TrycatchExceptionStartsHere.java
+++ TrycatchExceptionStartsHere.java
@@ -7,12 +7,14 @@ public class TrycatchExceptionStartsHere {
         System.out.println("Before dividing Using Zero");
 
         try {
-
-            int a=10/0;   //Arithmatic Exception is occured in line number 11.....
+        //The code having the Exception possibilities has to be written inside try block
+            int b=10/0;   //Arithmatic Exception is occured in line number 11.....
 
         }catch (Exception e) {
+
+             //If you get an Exception the control will move to the Catch block ...
             System.out.println("Exception is Handling in Catch Block");
         }
-        System.out.println("After ArithmaticException is Occured  ");
+        System.out.println("After ArithMaticException is Occured when Catch block is Handled the exception");
     }
 }
